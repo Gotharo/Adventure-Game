937cdf4c1c3111d3987b1a396d91c15715f18c4d
gotharo <gonzalo.castro@gotharo.cl>
2025-12-10 14:39:34 -0300

    Refactor: modularización de arrays y corrección de variables globales para playGame.js

---
+++ js/arrays.js
@@ -0,0 +1,22 @@
+export let map = [];
+map[0] = "An old stone keep.";
+map[1] = "A deep well.";
+map[2] = "A sunny glade.";
+map[3] = "A sleeping dragon.";
+map[4] = "A narrow pathway.";
+map[5] = "An ancient gate.";
+map[6] = "The edge of a river.";
+map[7] = "A lonely wooden bench.";
+map[8] = "An isolated cottage. Faint music comes from inside.";
+
+export let images = [];
+images[0] = "keep.png";
+images[1] = "well.png";
+images[2] = "glade.png";
+images[3] = "dragon.png";
+images[4] = "path.png";
+images[5] = "gate.png";
+images[6] = "river.png";
+images[7] = "bench.png";
+images[8] = "cottage.png";
+
--- js/script.js
+++ js/script.js
@@ -1,10 +1,82 @@
-let map = [];
-map[0] = "An old stone keep.";
-map[1] = "A deep well.";
-map[2] = "A sunny glade.";
-map[3] = "A sleeping dragon.";
-map[4] = "A narrow pathway.";
-map[5] = "An ancient gate.";
-map[6] = "The edge of a river.";
-map[7] = "A lonely wooden bench.";
-map[8] = "An isolated cottage. Faint music comes from inside.";
+import { images, map } from "./arrays.js";
+
+
+let mapLocation = 4;
+
+let output = document.getElementById("output");
+output.innerHTML = map[mapLocation];
+
+let gameMessage = "";
+
+let actionIKnow = ["north", "south", "east", "west"];
+let action = "";
+
+let buttonNorth = document.getElementById("north");
+let buttonSouth = document.getElementById("south");
+let buttonWest = document.getElementById("west");
+let buttonEast = document.getElementById("east");
+
+let locationImage = document.getElementById("locationImage");
+
+buttonNorth.addEventListener("click", clickHandler, false);
+buttonSouth.addEventListener("click", clickHandler, false);
+buttonWest.addEventListener("click", clickHandler, false);
+buttonEast.addEventListener("click", clickHandler, false);
+
+
+render();
+
+function clickHandler(event) {
+    playGame(event);
+}
+
+function playGame(event) {
+    gameMessage = "";
+    action = "";
+
+    if (event && event.target) {
+        const btnId = event.target.id;
+        switch (btnId) {
+            case "north":
+                action = "north";
+                break;
+
+            case "south":
+                action = "south";
+                break;
+            case "east":
+                action = "east";
+                break;
+            case "west":
+                action = "west";
+                break;
+        }
+        if (action) {
+            console.log(`player wants to go ${action}`);
+        }
+    }
+    switch (action) {
+        case "north":
+            mapLocation -= 3;
+            break;
+        case "east":
+            mapLocation += 1;
+            break;
+        case "south":
+            mapLocation += 3;
+            break;
+        case "west":
+            mapLocation -= 1;
+            break;
+    }
+
+    render();
+
+}
+
+
+function render() {
+    output.innerHTML = map[mapLocation];
+    locationImage.src = `./assets/img/${images[mapLocation]}`;
+    output.innerHTML += "<br><em>" + gameMessage + "</em>";
+}
